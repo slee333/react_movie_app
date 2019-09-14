5802b6d10022fb1110f1815603d10e0addd3e73f
Seung Wook Lee <seungwooklee94@gmail.com>
2019-09-14 22:44:20 +0900

    Updates

---
+++ precache-manifest.95cce457112cdf99714533e3046fc121.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "949cc87adcccc98033a8241ecfe321cc",
+    "url": "/react_movie_app/index.html"
+  },
+  {
+    "revision": "a1afa0e9e22baf991b40",
+    "url": "/react_movie_app/static/css/main.419aa4ce.chunk.css"
+  },
+  {
+    "revision": "afc5aafd7ae8e0ab56f8",
+    "url": "/react_movie_app/static/js/2.172fcc2f.chunk.js"
+  },
+  {
+    "revision": "a1afa0e9e22baf991b40",
+    "url": "/react_movie_app/static/js/main.cd2d3bca.chunk.js"
+  },
+  {
+    "revision": "9d250cdbdeccafd422b0",
+    "url": "/react_movie_app/static/js/runtime~main.3c3e3f62.js"
+  }
+]);
